06aade6291be009e6c0f4cad219df259cd8e9b7f
jakkavignesh <81904688+jakkavignesh@users.noreply.github.com>
2023-06-11 01:46:37 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,24 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function change(ev) {
+  ev.target.className += ' chng';
+}
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+    
+function drop(ev) {  
+    ev.preventDefault();
+  
+    var data = ev.dataTransfer.getData("text");
+  
+     ev.target.appendChild(document.getElementById(data));
+  
+}
+
+function reset() {
+  // redirectToPage('https://myfirstvignesh.000webhostapp.com/');
+  window.location.href = 'https://myfirstvignesh.000webhostapp.com/';
+}
